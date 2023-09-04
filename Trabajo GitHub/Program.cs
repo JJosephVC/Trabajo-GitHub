940377323a48eb4319253dc69722186c2aa40077
Joseph Valdivia Chavarría <josephvaldivia1908@gmail.com>
2023-09-04 16:48:37 -0600

    Mi parte

--- Trabajo GitHub/Program.cs
+++ Trabajo GitHub/Program.cs
@@ -1,1 +1,11 @@
-﻿// A trabajar amonooooossss
+﻿// Sistema de Registro de Datos de Usuario
+
+public class Registro
+{
+
+    void Datos()
+    {
+        Console.WriteLine("Datos del Usuario");
+    }
+
+}
