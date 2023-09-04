0cc0e9e959d5b00cc891819d77341b6d5277f639
EduardoJCentenoL <centenolopezeduaardojose@gmail.com>
2023-09-04 16:47:38 -0600

    Creé mi clase para la tarea(Eduardo Centeno)

--- Trabajo GitHub/Program.cs
+++ Trabajo GitHub/Program.cs
@@ -1,1 +1,12 @@
 ﻿// A trabajar amonooooossss
+
+public class Venta
+{
+    public void Vender()
+    {
+        Console.WriteLine("Vendiendo Producto...");
+        Console.WriteLine("Error en inventario:");
+        Console.WriteLine("-Producto no válido");
+        Console.WriteLine("-El producto no está en existencia");
+    }
+}
