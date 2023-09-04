58f6d43560df7415496a1ff2e81022a362ad3319
Jefry López <ljefry997@gmail.com>
2023-09-04 16:49:46 -0600

    El trabajo se termino

--- Trabajo GitHub/Program.cs
+++ Trabajo GitHub/Program.cs
@@ -1,1 +1,19 @@
-﻿// A trabajar amonooooossss
+﻿// Inventario elaborado por Jesus y Jefry
+
+
+public class ReporteInventario
+{
+
+    //Elaborado por Jefry
+    public void Inventario()
+    {
+        Console.WriteLine("Sistema de inventario");
+    }
+
+    //Elaborado por Jesus
+    public void inventario()
+    {
+         Console.WriteLine("Mostrar inventario");
+    }
+}
+
